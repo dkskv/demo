b72181bf73e18f82fd7b745998604d5da9e0e5f6
Dmitry Kuskov <dmtrkskv@gmail.com>
2022-07-16 14:53:51 +0300

    feat(draft): добавил connector для соединения dnd контейнеров

--- src/components/DroppableContainer/index.tsx
+++ src/components/DroppableContainer/index.tsx
@@ -1,65 +1,70 @@
-import { update } from "ramda";
-import { useCallback, useEffect, useState } from "react";
+import { remove, update } from "ramda";
+import { LegacyRef, useCallback, useState } from "react";
+import { IDndElement, useDndConnection } from "../../decorators/dndConnection";
 import { BoundingBox } from "../../utils/boundingBox";
-import { IObservable } from "../../utils/emitter";
 import { getBoxStyle, stretchStyle } from "../../utils/styles";
 import { DraggableBox } from "../DraggableBox";
 
-// todo: Возможно, вынести, также использовать в Sortable
-export interface IMovable {
-  key: string;
-  box: BoundingBox;
-}
-
 interface IDroppableContainerProps {
   id: string;
-
-  onMove(id: string, item: IMovable): void;
-  onDrop(id: string, item: IMovable): boolean; // потерян ли
-
-  // Подписка на события внешнего элемента
-
-  // overlapObservable: IObservable<IMovable>;
-  dropObservable: IObservable<IMovable>;
+  initialItems: IDndElement[];
 }
 
-// - Анимацией занимается целевой контейнер
-// - Обработкой перетаскивания и рендером перетаскиваемого занимается исходный контейнер
 export const DroppableContainer: React.VFC<IDroppableContainerProps> = ({
   id,
-  onDrop,
-  onMove,
-  dropObservable,
+  initialItems,
 }) => {
-  // todo: возможно, создать хук, н-р, useDrop
-
-  const handleOverlap = useCallback((item: IMovable) => {
-    // В начальной реализации ничего не делаем
+  const handleDragIn = useCallback((item: IDndElement) => {
+    return { canDrop: true };
   }, []);
 
-  const handleDrop = useCallback((item: IMovable) => {
+  const handleDropIn = useCallback((item: IDndElement) => {
     // Делаем анимацию через setTimeout, монтируем элемент
+    setItems((items) => {
+      return [item, ...items];
+    });
+
+    return { canDrop: true };
   }, []);
 
-  useEffect(() => {
-    dropObservable.subscribe(handleDrop);
-  }, [dropObservable, handleDrop]);
+  const { ref, onDrag, onDrop } = useDndConnection(id, {
+    onDragIn: handleDragIn,
+    onDropIn: handleDropIn,
+  });
 
-  const [items, setItems] = useState<IMovable[]>([
-    { key: "0", box: BoundingBox.createByDimensions(0, 0, 200, 200) },
-  ]);
+  const [items, setItems] = useState<IDndElement[]>(initialItems);
 
-  const handleMove = useCallback(
-    (item: IMovable) => {
+  const handleDrag = useCallback(
+    (item: IDndElement) => {
+      onDrag(id, item);
+
       setItems((prevState) => {
         const index = prevState.findIndex(({ key }) => key === item.key);
 
         return update(index, item, prevState);
       });
-
-      onMove(id, item);
     },
-    [id, onMove]
+    [id, onDrag]
+  );
+
+  // todo: срабатывает спустя 1-2 секунды после размонтирования. Зачем?
+  const handleDrop = useCallback(
+    (item: IDndElement) => {
+      // todo: приходит неактуальный item!
+
+      setItems((prevState) => {
+        const index = prevState.findIndex(({ key }) => key === item.key);
+
+        if (!~index) {
+          return prevState;
+        }
+
+        const { canDrop } = onDrop(id, prevState[index]);
+
+        return canDrop ? remove(index, 1, prevState) : prevState;
+      });
+    },
+    [id, onDrop]
   );
 
   return (
@@ -67,16 +72,24 @@ export const DroppableContainer: React.VFC<IDroppableContainerProps> = ({
       style={{
         ...getBoxStyle(BoundingBox.createByDimensions(0, 0, 200, 600)),
         background: "grey",
+        position: "relative",
       }}
+      ref={ref as LegacyRef<HTMLDivElement>}
     >
       {items.map((item) => (
         <DraggableBox
           key={item.key}
           value={item.box}
-          onChange={(box) => handleMove({ ...item, box })}
+          onChange={(box) => handleDrag({ ...item, box })}
+          onEnd={() => handleDrop(item)}
+          style={{ zIndex: 1 }}
         >
           <div
-            style={{ ...stretchStyle, background: "orange", cursor: "pointer" }}
+            style={{
+              ...stretchStyle,
+              background: "orange",
+              cursor: "pointer",
+            }}
           >
             {item.key}
           </div>
---
+++ src/decorators/dndConnection.tsx
@@ -0,0 +1,169 @@
+import { always, append, find, propEq, reject } from "ramda";
+import {
+  createContext,
+  useContext,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+  useRef,
+} from "react";
+import { BoundingBox } from "../utils/boundingBox";
+import { noop } from "../utils/common";
+import { getBoxOnPage } from "../utils/dom";
+
+export interface IDndElement {
+  key: string;
+  box: BoundingBox;
+}
+
+interface IResponse {
+  canDrop: boolean;
+}
+
+interface IInputEventHandler {
+  (a: IDndElement): IResponse;
+}
+
+/** Подписка на входящие элементы. Callback'и возвращают разрешение на вход */
+interface IInputConnection {
+  onDragIn: IInputEventHandler;
+  onDropIn: IInputEventHandler;
+}
+
+interface IOutputEventHandler {
+  (key: string, a: IDndElement): IResponse;
+}
+
+/** Оповещение об исходящих элементах. Callback'и возвращают разрешение на выход */
+interface IOutputConnection {
+  onDrag: IOutputEventHandler;
+  onDrop: IOutputEventHandler;
+}
+
+interface IDndContainer extends IInputConnection {
+  key: string;
+  element: Element; // мб все-таки ref?
+}
+
+interface IDndContext extends IOutputConnection {
+  register(a: IDndContainer): void;
+  unregister(key: string): void;
+}
+
+const defaultResponse: IResponse = { canDrop: false };
+
+export function useDndConnection(key: string, input: IInputConnection) {
+  const { onDrag, onDrop, register, unregister } = useContext(DndContext);
+
+  const ref = useRef<Element>();
+
+  // todo: доделать
+  useEffect(() => {
+    if (ref.current) {
+      register({ key, element: ref.current, ...input });
+
+      return () => unregister(key);
+    }
+  }, []);
+
+  return { ref, onDrag, onDrop } as const;
+}
+
+const DndContext = createContext<IDndContext>({
+  register: noop,
+  unregister: noop,
+  onDrag: always(defaultResponse),
+  onDrop: always(defaultResponse),
+});
+
+function isContainerOverlapWith(box: BoundingBox) {
+  return ({ element }: IDndContainer) =>
+    getBoxOnPage(element).intersectionArea(box) > 0;
+}
+
+/** Компонент выступает в роли сервера между контейнерами, которые хотят обменяться элементами */
+export const DndConnector: React.FC = ({ children }) => {
+  const [containers, setContainers] = useState<IDndContainer[]>([]);
+
+  const register = useCallback(
+    (item: IDndContainer) => setContainers(append(item)),
+    []
+  );
+
+  const unregister = useCallback(
+    (key: string) => setContainers(reject(propEq("key", key))),
+    []
+  );
+
+  /** Возвращает независимо позиционированный бокс для перемещаемого элемента и целевой контейнер */
+  const handleDndEvent = useCallback(
+    (containerKey: string, item: IDndElement) => {
+      const sourceContainer = find(propEq("key", containerKey), containers);
+
+      if (!sourceContainer) {
+        throw new Error(
+          `Контейнер с ключом ${containerKey} не зарегистрирован. Убедитесь, что ref связан с элементом`
+        );
+      }
+
+      const itemBoxOnPage = getBoxOnPage(sourceContainer.element).placeOutside(
+        item.box
+      );
+
+      const targetContainer = containers.find(
+        isContainerOverlapWith(itemBoxOnPage)
+      );
+
+      return sourceContainer === targetContainer
+        ? { itemBoxOnPage }
+        : { targetContainer, itemBoxOnPage };
+    },
+    [containers]
+  );
+
+  const onDrag = useCallback(
+    (containerKey: string, item: IDndElement) => {
+      const { itemBoxOnPage, targetContainer } = handleDndEvent(
+        containerKey,
+        item
+      );
+
+      if (!targetContainer) {
+        return defaultResponse;
+      }
+
+      return targetContainer.onDragIn({
+        ...item,
+        box: getBoxOnPage(targetContainer.element).placeInside(itemBoxOnPage),
+      });
+    },
+    [handleDndEvent]
+  );
+
+  const onDrop = useCallback(
+    (containerKey: string, item: IDndElement) => {
+      const { itemBoxOnPage, targetContainer } = handleDndEvent(
+        containerKey,
+        item
+      );
+
+      if (!targetContainer) {
+        return defaultResponse;
+      }
+
+      return targetContainer.onDropIn({
+        ...item,
+        box: getBoxOnPage(targetContainer.element).placeInside(itemBoxOnPage),
+      });
+    },
+    [handleDndEvent]
+  );
+
+  const value = useMemo<IDndContext>(
+    () => ({ register, unregister, onDrag, onDrop }),
+    [register, unregister, onDrag, onDrop]
+  );
+
+  return <DndContext.Provider value={value}>{children}</DndContext.Provider>;
+};
