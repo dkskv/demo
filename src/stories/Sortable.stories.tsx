da00cc2107de16c101fac54bdc7e73acd78cb97a
Dmitry Kuskov <dmtrkskv@gmail.com>
2022-07-17 23:02:05 +0300

    feat: удалил неактуальное демо

--- src/stories/Sortable.stories.tsx
+++ src/stories/Sortable.stories.tsx
@@ -1,14 +1,11 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { zipWith } from "ramda";
-import { useEffect, useState } from "react";
-import { DraggableBox } from "../components/DraggableBox";
 import { DroppableContainer } from "../components/DroppableContainer";
 import { SortableContainer } from "../components/SortableContainer";
 import { DndConnector } from "../decorators/dndConnection";
 import { BoundingBox } from "../utils/boundingBox";
-import { Point } from "../utils/point";
 import { ISortableItem, positionInChain } from "../utils/sortable/sortable";
-import { getBoxStyle, stretchStyle } from "../utils/styles";
+import { stretchStyle } from "../utils/styles";
 
 export default {
   title: "Demo/Sortable",
@@ -52,100 +49,6 @@ export const Only: ComponentStory<any> = () => {
     >
       {renderer}
     </SortableContainer>
-  );
-};
-
-// const [items1, setItems1] = useState(
-//   getItems(["yellow", "black", "lightblue"], [50, 150, 150])
-// );
-// const [items2, setItems2] = useState(
-//   getItems(["orange", "pink", "lightgreen"], [100, 150, 100])
-// );
-
-export const Double: ComponentStory<any> = () => {
-  const [box, setBox] = useState(
-    BoundingBox.createByDimensions(0, 0, width, 200)
-  );
-  const [isDropping, setIsDropping] = useState(false);
-
-  const [activeOuterIndex, setActiveOuterIndex] = useState(0);
-
-  const item = { key: "orange", box };
-
-  const outerBox1 = BoundingBox.createByDimensions(0, 0, width, 600);
-  const outerBox2 = BoundingBox.createByDimensions(700, 0, width, 600);
-  const ob = [outerBox1, outerBox2];
-
-  const handleEnd = () => {
-    setBox((prevBox) => {
-      // тут должен представить всех в одной системе координат
-
-      const realPrevBox =
-        activeOuterIndex === -1
-          ? prevBox
-          : prevBox.shift(ob[activeOuterIndex].origin);
-
-      for (let i = 0; i < ob.length; i++) {
-        const outerBox = ob[i];
-
-        if (realPrevBox.intersectionArea(outerBox) > 0) {
-          setActiveOuterIndex(i);
-          return realPrevBox.shift(ob[i].origin.negate());
-        }
-      }
-
-      setActiveOuterIndex(-1);
-      return realPrevBox;
-    });
-  };
-
-  useEffect(() => {
-    if (activeOuterIndex !== -1) {
-      // setTimeout для того, чтобы DOM успел застать два разных состояния
-      setTimeout(() => setBox((prevBox) => prevBox.moveTo(Point.nullish)));
-      setIsDropping(true);
-      setTimeout(() => setIsDropping(false), 300);
-    }
-  }, [activeOuterIndex]);
-
-  const db = (
-    <DraggableBox
-      key="lol"
-      value={box}
-      onChange={setBox}
-      onEnd={handleEnd}
-      style={
-        isDropping
-          ? { transitionDuration: "300ms", transitionProperty: "top left" }
-          : undefined
-      }
-    >
-      {renderer(item)}
-    </DraggableBox>
-  );
-
-  return (
-    <div style={{ display: "flex", columnGap: 500, position: "relative" }}>
-      <div
-        style={{
-          background: "grey",
-          position: "absolute",
-          ...getBoxStyle(outerBox1),
-        }}
-      >
-        {activeOuterIndex === 0 && db}
-      </div>
-      <div
-        style={{
-          background: "grey",
-          position: "absolute",
-          ...getBoxStyle(outerBox2),
-        }}
-      >
-        {activeOuterIndex === 1 && db}
-      </div>
-      {activeOuterIndex === -1 && db}
-    </div>
   );
 };
 
