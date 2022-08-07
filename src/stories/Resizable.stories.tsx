2d861e69f4c2bf129b77c79a12014a12ef70b618
Dmitry Kuskov <dmtrkskv@gmail.com>
2022-08-07 17:11:40 +0300

    feat: обновил стори для Resizable

--- src/stories/Resizable.stories.tsx
+++ src/stories/Resizable.stories.tsx
@@ -1,96 +1,114 @@
-import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { useCallback, useState } from "react";
+import { ComponentStory } from "@storybook/react";
+import { useCallback, useEffect, useState } from "react";
 import { Grid } from "../components/Grid";
 import Resizable from "../components/Resizable";
+import { Space } from "../components/Space";
 import { BoundingBox } from "../utils/boundingBox";
+import { magnetize } from "../utils/common";
 import { NumbersRange } from "../utils/numbersRange";
+import { Orientations } from "../utils/orientation";
 import { centererStyle, getBoxStyle, stretchStyle } from "../utils/styles";
 
-export default {
-  title: "Demo/Resizable",
-  component: Resizable,
-  parameters: {},
-} as ComponentMeta<typeof Resizable>;
+export default { title: "Demo" };
 
-const child = (
-  <span
-    style={{
-      ...stretchStyle,
-      ...centererStyle,
-      background: "purple",
-      color: "white",
-      cursor: "pointer",
-    }}
-  >
-    Resize me
-  </span>
-);
+export const ResizableBox: ComponentStory<typeof Resizable> = () => {
+  const gridStep = 20;
+  const gridLargeStep = 20 * 4;
 
-const initialValue = BoundingBox.createByDimensions(50, 60, 200, 150);
-const sizeBounds = {
-  width: new NumbersRange(100, 600),
-  height: new NumbersRange(80, 400),
-};
+  const [isGridEnabled, setIsGridEnabled] = useState(false);
+  const [isLockedRatio, setIsLockedRatio] = useState(false);
 
-const commonProps = {
-  sizeBounds,
-  children: child,
-};
+  const outerBox = BoundingBox.createByDimensions(0, 0, 800, 600).map(
+    magnetize(gridLargeStep)
+  );
+  const [value, setValue] = useState(() =>
+    BoundingBox.createByDimensions(50, 60, 200, 150)
+  );
 
-export const Simple: ComponentStory<typeof Resizable> = () => (
-  <Resizable {...commonProps} initialValue={initialValue} />
-);
-
-export const GridGlue: ComponentStory<typeof Resizable> = () => {
-  const mainStep = 20;
-
-  function magnetize(n: number) {
-    return Math.round(n / mainStep) * mainStep;
+  function magnetizeBox(b: BoundingBox) {
+    return b.map(magnetize(gridStep));
   }
 
-  const [value, setValue] = useState(initialValue.map(magnetize));
+  useEffect(() => {
+    if (isGridEnabled) {
+      setValue(magnetizeBox);
+    }
+  }, [isGridEnabled]);
 
   const handleEnd = useCallback(() => {
-    setValue((box: BoundingBox) => box.map(magnetize));
-  }, []);
+    if (isGridEnabled) {
+      setValue(magnetizeBox);
+    }
+  }, [isGridEnabled]);
 
   const grid = [
-    [mainStep, 1],
-    [mainStep * 4, 2],
-  ].map(([step, thickness], index) => (
-    <div
-      key={index}
-      style={{
-        position: "absolute",
-        opacity: 0.5,
-        ...getBoxStyle(
-          BoundingBox.createByDimensions(
-            -thickness / 2,
-            -thickness / 2,
-            700,
-            450
-          )
-        ),
-      }}
-    >
-      <Grid
-        id={String(index)}
-        step={step}
-        thickness={thickness}
-        color="lightgrey"
-      />
-    </div>
+    [gridStep, 1],
+    [gridLargeStep, 2],
+  ].map(([step, thickness]) => (
+    <Grid
+      key={step}
+      id={String(step)}
+      step={step}
+      thickness={thickness}
+      color="rgba(0, 0, 0, 0.5)"
+    />
   ));
 
   return (
-    <>
-      {grid}
-      <Resizable
-        {...commonProps}
-        value={value}
-        onChange={setValue}
-        onEnd={handleEnd}
-      />
-    </>
+    <Space size={40} orientation={Orientations.vertical}>
+      <Space size={20}>
+        <span>
+          <input
+            type="checkbox"
+            onChange={({ target: { checked } }) => {
+              setIsGridEnabled(checked);
+            }}
+          />
+          Grid
+        </span>
+        <span>
+          <input
+            type="checkbox"
+            onChange={({ target: { checked } }) => {
+              setIsLockedRatio(checked);
+            }}
+          />
+          Lock ratio
+        </span>
+      </Space>
+      <div
+        style={{
+          position: "relative",
+          border: "1px solid black",
+          ...getBoxStyle(outerBox),
+        }}
+      >
+        {isGridEnabled && grid}
+        <Resizable
+          value={value}
+          sizeBounds={{
+            width: new NumbersRange(100, 600),
+            height: new NumbersRange(80, 400),
+          }}
+          onEnd={handleEnd}
+          // todo: устранить необходимость вызывать `resetOrigin`
+          outerBox={outerBox.resetOrigin()}
+          onChange={setValue}
+          keepAspectRatio={isLockedRatio}
+        >
+          <span
+            style={{
+              ...stretchStyle,
+              ...centererStyle,
+              background: "purple",
+              color: "white",
+              cursor: "pointer",
+            }}
+          >
+            Resize me
+          </span>
+        </Resizable>
+      </div>
+    </Space>
   );
 };
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,7 +1,14 @@
+import { curryN } from "ramda";
+
 export interface IPressedKeys {
   shiftKey: boolean;
   altKey: boolean;
   ctrlKey: boolean;
 }
 
-export const noop = () => {}
+export const noop = () => {};
+
+export const magnetize = curryN(
+  2,
+  (step: number, n: number) => Math.round(n / step) * step
+);
