91720fe5f9087b36b22d32f37f24fc488eee6949
Dmitry Kuskov <dmtrkskv@gmail.com>
2022-11-03 01:34:50 +0300

    feat: переработал стори SortableCards

--- src/stories/SortableCards.stories.tsx
+++ src/stories/SortableCards.stories.tsx
@@ -1,13 +1,18 @@
 import { ComponentStory } from "@storybook/react";
 import { zipWith } from "ramda";
+import { useState } from "react";
+import { NumberInputWithSlider } from "../components/NumberInputWithSlider";
 import { SortableContainer } from "../components/SortableContainer";
 import {
   ISortableItem,
   positionInChain,
 } from "../components/SortableContainer/utils/sortable";
+import { Space } from "../components/Space";
 import { DndConnector } from "../decorators/dndConnection/dndConnector";
 import { useTheme } from "../decorators/theme";
 import { BoundingBox } from "../entities/boundingBox";
+import { Directions } from "../entities/direction";
+import { NumbersRange } from "../entities/numbersRange";
 import { stretchStyle } from "../utils/styles";
 
 export default {};
@@ -30,6 +35,7 @@ const generateItems = (colors: string[], sizes: number[]) =>
 
 export const SortableCards: ComponentStory<any> = () => {
   const theme = useTheme();
+  const [transitionDuration, setTransitionDuration] = useState(300);
 
   const items = generateItems(
     ["red", "blue", "green", "purple"],
@@ -60,46 +66,37 @@ export const SortableCards: ComponentStory<any> = () => {
     borderRadius: theme.largeBorderRadius,
   };
 
-  const mockWrapperStyle = {
-    padding: "10px",
-    border: `1px solid ${theme.strokeColor}`,
-    borderRadius: theme.largeBorderRadius,
-  };
-
   return (
-    <div style={{ display: "flex", columnGap: 280, position: "relative" }}>
-      <DndConnector>
-        <SortableContainer
-          id="first"
-          items={items}
-          transitionDuration={300}
-          box={BoundingBox.byDeltas(0, 0, width, 600)}
-          style={containerStyle}
-          renderItem={renderItem}
+    <Space size={50} direction={Directions.vertical}>
+      <Space size={4} direction={Directions.vertical} style={{ width: 400 }}>
+        Duration, ms
+        <NumberInputWithSlider
+          value={transitionDuration}
+          onChange={setTransitionDuration}
+          thickness={10}
+          bounds={new NumbersRange(100, 2000)}
         />
-        <SortableContainer
-          id="second"
-          items={items2}
-          transitionDuration={300}
-          box={BoundingBox.byDeltas(0, 0, width, 600)}
-          style={containerStyle}
-          renderItem={renderItem}
-        />
-        <div style={mockWrapperStyle}>
-          Nesting
-          <div style={mockWrapperStyle}>
-            Nesting
-            <SortableContainer
-              id="third"
-              items={[]}
-              transitionDuration={300}
-              box={BoundingBox.byDeltas(0, 0, width, 600)}
-              style={containerStyle}
-              renderItem={renderItem}
-            />
-          </div>
-        </div>
-      </DndConnector>
-    </div>
+      </Space>
+      <Space size={350} style={{ position: "relative" }}>
+        <DndConnector>
+          <SortableContainer
+            id="first"
+            items={items}
+            transitionDuration={transitionDuration}
+            box={BoundingBox.byDeltas(0, 0, width, 600)}
+            style={containerStyle}
+            renderItem={renderItem}
+          />
+          <SortableContainer
+            id="second"
+            items={items2}
+            transitionDuration={transitionDuration}
+            box={BoundingBox.byDeltas(0, 0, width, 600)}
+            style={containerStyle}
+            renderItem={renderItem}
+          />
+        </DndConnector>
+      </Space>
+    </Space>
   );
 };
